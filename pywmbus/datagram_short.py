3191cc223bd306bd1c7b7c02c8f0c949bb2bc1ac
jalmeroth <jan@almeroth.com>
2017-09-22 22:31:58 +0200

    sans_checksum rewrite

--- pywmbus/datagram_short.py
+++ pywmbus/datagram_short.py
@@ -18,49 +18,51 @@ def sans_checksum(data):
     """Verify checksums and remove them from data"""
     _LOGGER.debug("data: %s (%d)", data.hex(), len(data))
 
+    csum_len = 2
+    head_len = 9
+    body_len = data[0] - head_len
+    remainder = body_len % 16
+    times = int((body_len - remainder) / 16)
+    times += 1 if remainder > 0 else 0
+    full_len = ((times + 1) * csum_len) + data[0] + 1
+
+    _LOGGER.debug("%d: %d, %d = %d", len(data), times, remainder, full_len)
+
+    if len(data) < full_len:
+        raise WMBusDataLengthError("Not enough input data")
+
     # header consists of 10 bytes
     end = 10
-    # but leave L-Field aside
+    # copy over header but leave L-Field aside
     new_data = data[1:end]
-    # each 16th byte comes a checksum
-    times = int((len(data) / 16))
 
-    for i in range(1, times):
+    for i in range(0, times):
         start = end + 2
-        end = start + 16
-        _LOGGER.debug(
-            "%d:%d %s",
-            start,
-            end,
-            data[start:end].hex()
-        )
-        # self.data['_crc'].append(DatagramField(CRC_FIELD, data[10:12]))
-        csum_calc = _CRC_16(data[start:end])
-        csum_data = int.from_bytes(data[end:end + 2], byteorder="big")
-        _LOGGER.debug(
-            "csum: %s vs. %s",
-            hex(csum_calc),
-            hex(csum_data)
-        )
+        # last round, if remainder
+        if remainder > 0 and i == times - 1:
+            # calculate missing bytes
+            end = start + remainder
+        else:
+            end = start + 16  # 16 bytes data
 
-        if csum_calc != csum_data:
-            raise WMBusChecksumError("Checksum {} mismatch".format(i))
+        part = data[start:end]
 
-        new_data += data[start:end]
-    # last 2 bytes are a checksum too
-    start = end + 2
-    end = len(data) - 2
+        if part:
+            # self.data['_crc'].append(DatagramField(CRC_FIELD, data[10:12]))
+            _LOGGER.debug("%d %d:%d %s", i, start, end, part.hex())
 
-    csum_calc = _CRC_16(data[start:end])
-    csum_data = int.from_bytes(data[-2:], byteorder="big")
+            csum_calc = _CRC_16(part)
+            csum_data = int.from_bytes(data[end:end + 2], byteorder="big")
 
-    if csum_calc != csum_data:
-        raise WMBusChecksumError("Checksum mismatch")
+            _LOGGER.debug("csum: %s vs. %s", hex(csum_calc), hex(csum_data))
 
-    # append all the rest that is left
-    new_data += data[start:end]
+            if csum_calc != csum_data:
+                raise WMBusChecksumError("Checksum {} mismatch".format(i))
+
+            new_data += data[start:end]
+
     if data[0] != len(new_data):
-        raise WMBusDataLengthError()
+        raise WMBusDataLengthError("Not enough output data")
 
     _LOGGER.debug("new_data: %s (%d)", new_data.hex(), len(new_data))
     return new_data
